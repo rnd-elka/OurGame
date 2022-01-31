cec429fc3395feb5b4df60132b39966cdc3f869b
michalAchirnia <98042412+michalAchirnia@users.noreply.github.com>
2022-01-31 18:58:34 +0200

    function MakeQustion

--- questions.js
+++ questions.js
@@ -1,1 +1,10 @@
-var questions=[];
+var questions = [];
+
+function MakeQuestion(question, answer1, answer2, answer3, correctAnswer) {
+    obj = {
+        question: question,
+        answer: [answer1, answer2, answer3],
+        correctAnswer: correctAnswer
+    }
+    return obj;
+}
